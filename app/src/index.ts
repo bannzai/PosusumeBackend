c7e0f98e0f077a9b5c93b152d93bdd8a92803e64
bannzai <kingkong999yhirose@gmail.com>
2021-08-13 21:54:11 +0900

    :memo: Overwrite with auth

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,8 +1,10 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer, AuthenticationError } from "apollo-server";
 import { loadSchemaSync } from "@graphql-tools/load";
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 import { addResolversToSchema } from "@graphql-tools/schema";
 import { join } from "path";
+import { Resolvers } from "./types/generated/graphql";
+import { Context } from "./types/context";
 
 // サンプルデータの定義
 const books = [
@@ -21,17 +23,44 @@ const schema = loadSchemaSync(join(__dirname, "../schemas/schema.graphql"), {
   loaders: [new GraphQLFileLoader()],
 });
 
-// リゾルバーの定義
-const resolvers = {
+// リゾルバーの定義 (型のサポートを受けれる)
+const resolvers: Resolvers = {
   Query: {
-    books: () => books,
+    books: (_parent, _args, _context) => {
+      // TODO: 詳細な認可処理を行う
+
+      return books;
+    },
   },
 };
 
 const schemaWithResolvers = addResolversToSchema({ schema, resolvers });
 
+const getUser = (token?: string): Context["user"] => {
+  if (token === undefined) {
+    throw new AuthenticationError(
+      "認証されていないユーザーはリソースにアクセスできません"
+    );
+  }
+
+  // TODO: Tokenからユーザー情報を取り出す処理
+
+  return {
+    name: "dummy name",
+    email: "dummy@example.com",
+    token,
+  };
+};
+
 // サーバーの起動
-const server = new ApolloServer({ schema: schemaWithResolvers });
+const server = new ApolloServer({
+  schema: schemaWithResolvers,
+  context: ({ req }) =>
+    ({
+      user: getUser(req.headers.authorization),
+    } as Context),
+  debug: false, // エラーレスポンスにスタックトレースを含ませない、開発環境ではtrueにした方が分析が捗りそう
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
