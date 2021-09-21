8e34313bbf4eef91eda43694284feedd163140d2
bannzai <kingkong999yhirose@gmail.com>
2021-09-21 14:54:09 +0900

    :recycle: avoid compile error

--- app/src/index.ts
+++ app/src/index.ts
@@ -3,7 +3,7 @@ import { loadSchemaSync } from "@graphql-tools/load";
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 import { addResolversToSchema } from "@graphql-tools/schema";
 import { join } from "path";
-import { Context, setUserIDForMe } from "./types/context";
+import { Context } from "./types/context";
 
 import admin = require("firebase-admin");
 import express = require("express");
@@ -31,3 +31,37 @@ const port = process.env.PORT || 8081;
 server.listen(port).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
+
+const setUserIDForMe = async (
+  request: express.Request
+): Promise<Context["me"]> => {
+  if (
+    process.env["APP_ENVIRONMENT"] === "DEVELOPMENT" &&
+    process.env["APP_FIREBASE_AUTH_TEST_USER_ID"] != null
+  ) {
+    return {
+      userID: process.env["APP_FIREBASE_AUTH_TEST_USER_ID"],
+    };
+  }
+
+  const authorization = request.headers.authorization;
+  if (authorization == null) {
+    return null;
+  }
+  const splited = authorization.split(" ");
+  if (splited.length !== 2) {
+    return null;
+  }
+  const bearer = splited[0];
+  if (bearer !== "Bearer") {
+    return null;
+  }
+
+  const token = splited[1];
+  const decodedToken = await admin.auth().verifyIdToken(token);
+  const userID = decodedToken.uid;
+
+  return {
+    userID: userID,
+  };
+};
--- app/src/types/context.d.ts
+++ app/src/types/context.d.ts
@@ -5,37 +5,3 @@ export type Context = {
   me: Me | null;
   database: admin.firestore;
 };
-
-export const setUserIDForMe = async (
-  request: express.Request
-): Promise<Context["me"]> => {
-  if (
-    process.env["APP_ENVIRONMENT"] === "DEVELOPMENT" &&
-    process.env["APP_FIREBASE_AUTH_TEST_USER_ID"] != null
-  ) {
-    return {
-      userID: process.env["APP_FIREBASE_AUTH_TEST_USER_ID"],
-    };
-  }
-
-  const authorization = request.headers.authorization;
-  if (authorization == null) {
-    return null;
-  }
-  const splited = authorization.split(" ");
-  if (splited.length !== 2) {
-    return null;
-  }
-  const bearer = splited[0];
-  if (bearer !== "Bearer") {
-    return null;
-  }
-
-  const token = splited[1];
-  const decodedToken = await admin.auth().verifyIdToken(token);
-  const userID = decodedToken.uid;
-
-  return {
-    userID: userID,
-  };
-};
