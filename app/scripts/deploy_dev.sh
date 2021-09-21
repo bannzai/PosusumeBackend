b51ffabab2ec98e2f02748a4e70749d327388729
bannzai <kingkong999yhirose@gmail.com>
2021-09-21 15:52:49 +0900

    :memo: add deploy command

---
+++ app/scripts/deploy_dev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+set -x
+
+COMMIT_HASH=$(git rev-parse HEAD)
+DOCKER_IMAGE="$APP_DEVELOPMENT_DOCKER_IMAGE_BASE_PATH:$COMMIT_HASH"
+echo "docker build name and push to $DOCKER_IMAGE"
+
+docker build -t "$DOCKER_IMAGE" --target runner .
+docker push "$DOCKER_IMAGE"
+gcloud run deploy $APP_DEV_SERVICE_NAME \
+  --image $DOCKER_IMAGE \
+  --port $APP_DEV_SERVER_PORT \
+  --project $APP_DEV_GCP_PROJECT_ID \
+  --region $APP_DEV_GCP_REGION \
+  --allow-unauthenticated \
+  --quiet
