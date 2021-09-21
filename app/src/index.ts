2818f079a258887f287a7267cb3784cbd12fcfe9
bannzai <kingkong999yhirose@gmail.com>
2021-09-21 14:42:18 +0900

    :recycle: Extract to context

--- app/src/index.ts
+++ app/src/index.ts
@@ -3,7 +3,7 @@ import { loadSchemaSync } from "@graphql-tools/load";
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 import { addResolversToSchema } from "@graphql-tools/schema";
 import { join } from "path";
-import { Context } from "./types/context";
+import { Context, setUserIDForMe } from "./types/context";
 
 import admin = require("firebase-admin");
 import express = require("express");
@@ -28,40 +28,6 @@ const schema = loadSchemaSync(join(__dirname, "../schemas/schema.graphql"), {
 });
 
 const schemaWithResolvers = addResolversToSchema({ schema, resolvers });
-
-const setUserIDForMe = async (
-  request: express.Request
-): Promise<Context["me"]> => {
-  if (
-    process.env["APP_ENVIRONMENT"] === "DEVELOPMENT" &&
-    process.env["APP_FIREBASE_AUTH_TEST_USER_ID"] != null
-  ) {
-    return {
-      userID: process.env["APP_FIREBASE_AUTH_TEST_USER_ID"],
-    };
-  }
-
-  const authorization = request.headers.authorization;
-  if (authorization == null) {
-    return null;
-  }
-  const splited = authorization.split(" ");
-  if (splited.length !== 2) {
-    return null;
-  }
-  const bearer = splited[0];
-  if (bearer !== "Bearer") {
-    return null;
-  }
-
-  const token = splited[1];
-  const decodedToken = await admin.auth().verifyIdToken(token);
-  const userID = decodedToken.uid;
-
-  return {
-    userID: userID,
-  };
-};
 
 const server = new ApolloServer({
   schema: schemaWithResolvers,
