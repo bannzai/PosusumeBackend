1e55c9a271f5930e009a7291156a5d049e32bf2b
bannzai <kingkong999yhirose@gmail.com>
2021-10-14 04:43:45 +0900

    :sparkels: add function of writeSpotImageURLOnResized

--- app/functions/src/index.ts
+++ app/functions/src/index.ts
@@ -1,9 +1,4 @@
-import * as functions from "firebase-functions";
+import admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.writeSpotImageURLOnResized = require("./functions/writeSpotImageURLOnResized/function");
