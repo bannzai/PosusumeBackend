20787616c130f8fcd6967fc84f865e91ae2efff0
bannzai <kingkong999yhirose@gmail.com>
2021-10-17 10:03:52 +0900

    :recycle: remove unnecessary import

--- app/functions/src/setUserID/function.ts
+++ app/functions/src/setUserID/function.ts
@@ -1,6 +1,5 @@
 import * as functions from "firebase-functions";
 import admin = require("firebase-admin");
-import { Spot } from "../../../src/types/generated/graphql";
 
 const database = admin.firestore();
 
