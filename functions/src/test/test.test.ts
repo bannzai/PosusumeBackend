bd50b4ac0fc94309a2c8e2ca45fe2886ee6cbbe8
bannzai <kingkong999yhirose@gmail.com>
2021-11-01 17:37:24 +0900

    :sparkles: add firestore test

--- functions/src/test/test.test.ts
+++ functions/src/test/test.test.ts
@@ -1,4 +1,10 @@
+process.env.FIRESTORE_EMULATOR_HOST = "localhost:8089";
+
 import { expect } from "chai";
+
+import * as admin from "firebase-admin";
+admin.initializeApp();
+const database = admin.firestore();
 
 describe("#test", () => {
   it("success", () => {
@@ -6,5 +12,18 @@ describe("#test", () => {
   });
   it("success 2", () => {
     expect(1).to.be.equal(1);
+  });
+});
+
+describe("test with database", () => {
+  it("should write document successfully", async () => {
+    await database
+      .doc("users/test_user_identifier/spots/test_spot_identifier")
+      .set({ dummy: true });
+
+    const snapshot = await database
+      .doc("users/test_user_identifier/spots/test_spot_identifier")
+      .get();
+    expect(snapshot.data()!.dummy).to.equal(true);
   });
 });
