8e299600b67fdb69923289048fd01acb9f46e247
bannzai <kingkong999yhirose@gmail.com>
2021-11-01 16:52:43 +0900

    :recycle: fix test case

--- functions/src/test/test.test.ts
+++ functions/src/test/test.test.ts
@@ -4,7 +4,7 @@ describe("#test", () => {
   it("success", () => {
     expect(1).to.be.equal(1);
   });
-  it("failure", () => {
-    expect(0).to.be.equal(1);
+  it("success 2", () => {
+    expect(1).to.be.equal(1);
   });
 });
