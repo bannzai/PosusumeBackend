646d807cd1a06c9a2e1452a51a207d8b3899d1c4
bannzai <kingkong999yhirose@gmail.com>
2021-11-06 08:38:41 +0900

    :recycle: Replace to relative path

--- packages/functions/src/functions/writeSpotImageURLOnResized/function.ts
+++ packages/functions/src/functions/writeSpotImageURLOnResized/function.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions";
 import admin = require("firebase-admin");
-import { Spot } from "@posusume/shared";
+import { Spot } from "../../../../shared/types/generated/graphql";
 
 const database = admin.firestore();
 
