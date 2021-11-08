25671b69c7ceab5d38ff794e538090ce0df3a0b9
bannzai <kingkong999yhirose@gmail.com>
2021-11-08 10:39:06 +0900

    :recycle: separate config

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,27 +1,10 @@
 import { ApolloServer } from "apollo-server";
-import { loadSchemaSync } from "@graphql-tools/load";
-import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
-import { addResolversToSchema } from "@graphql-tools/schema";
-import { join } from "path";
 import admin = require("firebase-admin");
-import { resolvers } from "./resolvers/resolver";
-import { setUserIDForMe } from "@posusume/graphql";
+import { config } from "./config";
 
 admin.initializeApp();
 
-const schema = loadSchemaSync(join(__dirname, "../../../schemas/schema.graphql"), {
-  loaders: [new GraphQLFileLoader()],
-});
-
-const server = new ApolloServer({
-  schema: addResolversToSchema({ schema, resolvers }),
-  introspection: process.env["APP_ENVIRONMENT"] === "DEVELOPMENT",
-  context: async (expressContext) => ({
-    me: await setUserIDForMe(expressContext.req),
-    database: admin.firestore(),
-  }),
-  debug: process.env["APP_ENVIRONMENT"] === "DEVELOPMENT",
-});
+const server = new ApolloServer(config);
 
 const port = process.env.PORT || 8081;
 server.listen(port).then(({ url }) => {
