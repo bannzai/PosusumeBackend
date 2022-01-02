134d3500a7fd66032304e7a74f9f350256d753e1
bannzai <kingkong999yhirose@gmail.com>
2022-01-02 15:18:55 +0900

    :sparkles: add functions to index

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -3,3 +3,4 @@ admin.initializeApp();
 
 exports.writeSpotImageURLOnResized = require("./functions/writeSpotImageURLOnResized/function");
 exports.setUserID = require("./functions/setUserID/function");
+exports.emotionalise = require("./functions/emotionalise/function");
