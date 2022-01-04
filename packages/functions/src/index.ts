a7a9a755a9f2a1c00ae0b7c1b6889aa36b8ebab6
bannzai <kingkong999yhirose@gmail.com>
2022-01-05 00:04:43 +0900

    :recycle: Rename to detectLabel

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -3,4 +3,4 @@ admin.initializeApp();
 
 exports.writeSpotImageURLOnResized = require("./functions/writeSpotImageURLOnResized/function");
 exports.setUserID = require("./functions/setUserID/function");
-exports.emotionalise = require("./functions/emotionalise/function");
+exports.detectLabel = require("./functions/detectLabel/function");
