19a46aef98d9d64da7b27d21b67c17eaf56b953e
bannzai <kingkong999yhirose@gmail.com>
2022-01-09 22:21:27 +0900

    :recycle: missing path

--- packages/api/src/config.ts
+++ packages/api/src/config.ts
@@ -8,7 +8,7 @@ import { setUserIDForMe } from "@posusume/graphql";
 import { ApolloServerExpressConfig } from "apollo-server-express";
 
 const schema = loadSchemaSync(
-  join(__dirname, "../../../graphql/schemas/schema.graphql"),
+  join(__dirname, "../../../packages/graphql/schemas/schema.graphql"),
   {
     loaders: [new GraphQLFileLoader()],
   }
