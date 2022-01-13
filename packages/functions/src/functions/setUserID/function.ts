0336f9af1ea317ec2d223960406ba761d4894219
bannzai <kingkong999yhirose@gmail.com>
2022-01-13 21:29:14 +0900

    :memo: using uuidv4

--- packages/functions/src/functions/setUserID/function.ts
+++ packages/functions/src/functions/setUserID/function.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import admin = require("firebase-admin");
 import { User } from "@posusume/graphql/types/generated/graphql";
+import { v4 as uuidv4 } from "uuid";
 
 const database = admin.firestore();
 
@@ -8,7 +9,7 @@ module.exports = functions.auth.user().onCreate(async (authUser) => {
   const id = authUser.uid;
   const user: User = {
     id,
-    name: id,
+    name: uuidv4(),
     resizedProfileImageURLs: {},
   };
   await database.doc(`users/${id}`).set(user, { merge: true });
