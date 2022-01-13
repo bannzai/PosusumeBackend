19e60f94ccacefe1eb4d268c30a1bb20944e2e5e
bannzai <kingkong999yhirose@gmail.com>
2022-01-13 18:57:32 +0900

    :sparkles: add user resolver

--- packages/api/src/domain/me/resolver.ts
+++ packages/api/src/domain/me/resolver.ts
@@ -1,6 +1,12 @@
-import { MeResolvers } from "@posusume/graphql/types/generated/graphql";
+import { MeResolvers, User } from "@posusume/graphql/types/generated/graphql";
 
 export const meResolver: MeResolvers = {
+  user: async (_parent, _args, _context) => {
+    const { database } = _context;
+    const document = await database.doc(`users/${_parent.id}`).get();
+    const user = document.data() as User;
+    return user;
+  },
   spots: async (_parent, _args, _context) => {
     const spotDocumentReferences = await _context.database
       .collection(`users/${_parent.id}/spots`)
