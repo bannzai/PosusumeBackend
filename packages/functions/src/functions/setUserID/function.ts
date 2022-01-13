95af3997b663b03cc5faefee2b40be696dbd2823
bannzai <kingkong999yhirose@gmail.com>
2022-01-13 21:26:35 +0900

    :sparkles: add user properties

--- packages/functions/src/functions/setUserID/function.ts
+++ packages/functions/src/functions/setUserID/function.ts
@@ -1,9 +1,15 @@
 import * as functions from "firebase-functions";
 import admin = require("firebase-admin");
+import { User } from "@posusume/graphql/types/generated/graphql";
 
 const database = admin.firestore();
 
-module.exports = functions.auth.user().onCreate(async (user) => {
-  const id = user.uid;
-  await database.doc(`users/${id}`).set({ id }, { merge: true });
+module.exports = functions.auth.user().onCreate(async (authUser) => {
+  const id = authUser.uid;
+  const user: User = {
+    id,
+    name: id,
+    resizedProfileImageURLs: {},
+  };
+  await database.doc(`users/${id}`).set(user, { merge: true });
 });
